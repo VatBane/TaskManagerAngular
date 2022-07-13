c6ae232bca023a3751e5a88760f009632dc48875
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-13 13:15:58 +0300

    simple getting data

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
 
-@Component ({
-  selector: "card",
-  templateUrl: "./card.component.html",
+@Component({
+  selector: 'card',
+  templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
+export class CardComponent implements OnInit {
+  tasks: any;
 
-export class CardComponent {}
+  constructor(private service: PostService) {}
+
+  ngOnInit(): void {
+    this.service.getTasks().subscribe(response => {
+      this.tasks = response;
+    });
+  }
+}
