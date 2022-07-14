09230ce5283087576d277b2cf8811de29421208e
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-14 13:59:13 +0300

    cards style

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,14 +11,12 @@ export class CardComponent implements OnInit{
 
   @Input() public task: any;
 
-  public imgUrl: String = '';
   public name: String = '';
   public status: String = '';
 
   constructor() { }
 
   ngOnInit () {
-    this.imgUrl = '../../assets/img/' + this.task.completed + '.png';
     this.name = this.task.name;
     this.status = this.task.completed;
   }
