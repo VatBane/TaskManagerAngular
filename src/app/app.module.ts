631fc446366e9b9ad8f4835b1343bb0934e320e1
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-18 13:04:30 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule} from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ListComponent } from './list/list.component';
 import { MainComponent } from './main/main.component';
+import { TaskComponent } from './task/task.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     ListComponent,
-    MainComponent
+    MainComponent,
+    TaskComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
