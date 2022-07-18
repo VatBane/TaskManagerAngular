77d6a61c5ccffb79f93e3e2dd8442c756556da01
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-18 15:36:43 +0300

    update Task

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -26,8 +26,40 @@ export class TaskComponent implements OnInit {
       id = params.get('id');
     });
 
-    this.task = await this.getInfo(id);    
+    this.task = await this.getInfo(id);  
     console.log(this.task);
+  }
+
+  async submitTask(event: any) {
+    event.target.blur();
+
+    if (!this.task.name) {
+      alert('Please, enter your task');
+      return;
+    }
+    
+    const res = this.service.updateTask({
+      "name": this.task.name,
+      "completed": this.task.completed
+    }, this.task._id);
+
+    let task: any;
+    task = await lastValueFrom(res);
+
+    this.task.name = task.task.name;
+    this.task._id = task.task._id;
+
+    console.log(task);
+    console.log(this.task);
+  }
+
+  async changeStatus() {
+    if (this.task.completed == true) {
+      this.task.completed = false;
+    }
+    else {
+      this.task.completed = true;
+    }
   }
 
 }
